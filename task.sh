55c0f2e4c1c2380ce98b2ac74120ff9549ce2b54
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2025-03-13 20:35:48 +0200

    fnish

--- task.sh
+++ task.sh
@@ -1,1 +1,10 @@
 #! /bin/bash
+
+DB_USER=${DB_USER}
+DB_PASSWORD=${DB_PASSWORD}
+
+mysqldump -u ${DB_USER} -p${DB_PASSWORD} --databases ShopDB  --result-file=backup-new.sql --no-create-db;
+mysql -u ${DB_USER} -p${DB_PASSWORD} ShopDBReserve < backup-new.sql;
+
+mysqldump -u ${DB_USER} -p${DB_PASSWORD} --databases ShopDB  --result-file=backup-new.sql --no-create-info;
+mysql -u ${DB_USER} -p${DB_PASSWORD} ShopDBDevelopment < backup-new.sql;
